9601915245e0cbf6e2bde8e89a5b4202ef16b933
Gabriel Lee <chihonglee777@gmail.com>
2026-03-05 22:28:47 +0800

    feat: add lane stats to debug panel

--- main.py
+++ main.py
@@ -3,7 +3,29 @@ import math
 import dearpygui.dearpygui as dpg
 from collections import defaultdict
 
-RENDER_SCALE = 8
+def get_ore_production_in_lane(lane):
+    counter = {
+        "coal": 0,
+        "iron-ore": 0,
+        "copper-ore": 0,
+        "stone": 0,
+    }
+
+    for pair in lane.values():
+        for side in pair.values():
+            for ore, count in side["ore_ratio"].items():
+                counter[ore] += count
+
+    return counter
+
+def check_lane_has_at_least_30_miners(lane):
+    miner_count = 0
+    for pair in lane.values():
+        for side in pair.values():
+            miner_count += 1
+            if miner_count >= 30:
+                return True
+    return False
 
 file = open('ores.txt', 'r')
 if not file:
@@ -69,12 +91,33 @@ for direction in ["horizontal", "vertical"]:
 
 # GUI
 
+ui_configs = {
+    "large": {
+        "width": 1800,
+        "height": 1400,
+        "x_pos": 700,
+        "y_pos": 400,
+        "font_size": 20,
+        "render_scale": 8,
+    },
+    "small": {
+        "width": 1500,
+        "height": 1000,
+        "x_pos": 0,
+        "y_pos": 0,
+        "font_size": 15,
+        "render_scale": 6,
+    },
+}
+
+ui_config = ui_configs["small"]
+
 dpg.create_context()
-dpg.create_viewport(width=1800, height=1400, x_pos=700, y_pos=400, title="Weeeee", clear_color=(30, 30, 30))
+dpg.create_viewport(width=ui_config["width"], height=ui_config["height"], x_pos=ui_config["x_pos"], y_pos=ui_config["y_pos"], title="Weeeee", clear_color=(30, 30, 30))
 dpg.setup_dearpygui()
 
 with dpg.font_registry():
-    default_font = dpg.add_font("FiraMono-Regular.ttf", 20)
+    default_font = dpg.add_font("FiraMono-Regular.ttf", ui_config["font_size"])
     dpg.bind_font(default_font)
 
 debug_info = {
@@ -95,7 +138,7 @@ def update_miners_display():
     # Draw background
     dpg.draw_rectangle(
         (0, 0),
-        (bounding_box_size[0] * RENDER_SCALE, bounding_box_size[1] * RENDER_SCALE),
+        (bounding_box_size[0] * ui_config["render_scale"], bounding_box_size[1] * ui_config["render_scale"]),
         fill=(64, 64, 64),
         parent="map_drawlist"
     )
@@ -113,8 +156,8 @@ def update_miners_display():
         else:
             continue
         dpg.draw_rectangle(
-            ((position[0] - bounding_box[0][0]) * RENDER_SCALE, (position[1] - bounding_box[0][1]) * RENDER_SCALE),
-            ((position[0] - bounding_box[0][0] + 1) * RENDER_SCALE, (position[1] - bounding_box[0][1] + 1) * RENDER_SCALE),
+            ((position[0] - bounding_box[0][0]) * ui_config["render_scale"], (position[1] - bounding_box[0][1]) * ui_config["render_scale"]),
+            ((position[0] - bounding_box[0][0] + 1) * ui_config["render_scale"], (position[1] - bounding_box[0][1] + 1) * ui_config["render_scale"]),
             color=color,
             fill=color,
             parent="map_drawlist"
@@ -127,8 +170,8 @@ def update_miners_display():
             for side in pair.values():
                 has_miner_in_lane = True
                 dpg.draw_rectangle(
-                    ((side["position"][0] - bounding_box[0][0]) * RENDER_SCALE, (side["position"][1] - bounding_box[0][1]) * RENDER_SCALE),
-                    ((side["position"][0] + 3 - bounding_box[0][0]) * RENDER_SCALE, (side["position"][1] + 3 - bounding_box[0][1]) * RENDER_SCALE),
+                    ((side["position"][0] - bounding_box[0][0]) * ui_config["render_scale"], (side["position"][1] - bounding_box[0][1]) * ui_config["render_scale"]),
+                    ((side["position"][0] + 3 - bounding_box[0][0]) * ui_config["render_scale"], (side["position"][1] + 3 - bounding_box[0][1]) * ui_config["render_scale"]),
                     color=(255, 255, 255, 128),
                     fill=(255, 255, 255, 32),
                     parent="map_drawlist"
@@ -141,20 +184,20 @@ def update_miners_display():
             }
             if direction == "horizontal":
                 dpg.draw_rectangle(
-                    (0, (lane_index * lane_width + offset_y + 3) * RENDER_SCALE),
-                    (bounding_box_size[0] * RENDER_SCALE, (lane_index * lane_width + offset_y + 3 + 1) * RENDER_SCALE),
+                    (0, (lane_index * lane_width + offset_y + 3) * ui_config["render_scale"]),
+                    (bounding_box_size[0] * ui_config["render_scale"], (lane_index * lane_width + offset_y + 3 + 1) * ui_config["render_scale"]),
                     **lane_args
                 )
             else:
                 dpg.draw_rectangle(
-                    ((lane_index * lane_width + offset_x + 3) * RENDER_SCALE, 0),
-                    ((lane_index * lane_width + offset_x + 3 + 1) * RENDER_SCALE, bounding_box_size[1] * RENDER_SCALE),
+                    ((lane_index * lane_width + offset_x + 3) * ui_config["render_scale"], 0),
+                    ((lane_index * lane_width + offset_x + 3 + 1) * ui_config["render_scale"], bounding_box_size[1] * ui_config["render_scale"]),
                     **lane_args
                 )
 
 def click_handler():
     click_position = dpg.get_drawing_mouse_pos()
-    position = (click_position[0] // RENDER_SCALE + bounding_box[0][0], click_position[1] // RENDER_SCALE + bounding_box[0][1])
+    position = (click_position[0] // ui_config["render_scale"] + bounding_box[0][0], click_position[1] // ui_config["render_scale"] + bounding_box[0][1])
 
     direction = dpg.get_value("direction_combo")
     offset_x = dpg.get_value("offset_x_slider")
@@ -163,28 +206,33 @@ def click_handler():
     debug_info["position"] = position
     debug_info["ore"] = map[position] if position in map else ""
     debug_info["miner"] = None
+    ore_production_in_lane = {}
     for lane in miners[direction][offset_x][offset_y].values():
         for pair in lane.values():
             for side in pair.values():
                 miner_position = side["position"]
                 if miner_position[0] <= position[0] < miner_position[0] + 3 and miner_position[1] <= position[1] < miner_position[1] + 3:
                     debug_info["miner"] = side
+                    debug_info["lane"] = lane
+
+                    ore_production_in_lane = get_ore_production_in_lane(lane)
                     break
     dpg.set_value("position_text", "position: " + str(debug_info["position"]))
     dpg.set_value("ore_text", "ore: " + str(debug_info["ore"]))
     dpg.set_value("miner_position_text", "  position: " + str(debug_info["miner"]["position"] if debug_info["miner"] else ""))
     dpg.set_value("miner_ore_ratio_text", "  ore ratio:\n    " + ("\n    ".join([f"{k}: {v}" for k, v in debug_info["miner"]["ore_ratio"].items()]) if debug_info["miner"] else ""))
+    dpg.set_value("lane_ore_production_text", "  ore count:\n    " + ("\n    ".join([f"{k}: {v}" for k, v in ore_production_in_lane.items()]) if ore_production_in_lane else "")) 
 
 with dpg.window(label="Map", autosize=True):
     with dpg.drawlist(
-        width=bounding_box_size[0] * RENDER_SCALE,
-        height=bounding_box_size[1] * RENDER_SCALE,
+        width=bounding_box_size[0] * ui_config["render_scale"],
+        height=bounding_box_size[1] * ui_config["render_scale"],
         callback=click_handler,
         tag="map_drawlist"
     ):
         pass
 
-with dpg.window(label="Debug", pos=(bounding_box_size[0] * RENDER_SCALE + 30, 0), autosize=True):
+with dpg.window(label="Debug", pos=(bounding_box_size[0] * ui_config["render_scale"] + 30, 0), autosize=True):
     dpg.add_combo(label="Direction", items=["vertical", "horizontal"], default_value="vertical", tag="direction_combo", callback=lambda: update_miners_display())
     dpg.add_slider_int(label="Offset X", min_value=0, max_value=6, tag="offset_x_slider", callback=lambda: update_miners_display())
     dpg.add_slider_int(label="Offset Y", min_value=0, max_value=6, tag="offset_y_slider", callback=lambda: update_miners_display())
@@ -194,6 +242,8 @@ with dpg.window(label="Debug", pos=(bounding_box_size[0] * RENDER_SCALE + 30, 0)
     dpg.add_text("miner:")
     dpg.add_text("  position: ", tag="miner_position_text")
     dpg.add_text("  ore ratio: ", tag="miner_ore_ratio_text")
+    dpg.add_text("lane:")
+    dpg.add_text("  ore production: ", tag="lane_ore_production_text")
 
 # Initial render
 update_miners_display()
